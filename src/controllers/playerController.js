162034d578de0c0badb4ad15d276a30d1dcc7cfb
heshan <heshansandakelum2018@gmail.com>
2026-06-27 07:27:29 +0530

    adding CRUD part to Player Controller and Routes all

--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -17,4 +17,93 @@ const createPlayer=async (req,res)=>{
 
     }
 };
-module.exports={createPlayer};
+
+const getAllPlayers=async(req,res)=>{
+    try{
+        const players=await Player.find();
+        res.status(200).json({
+            success:true,
+            count:players.length,
+            data:players
+        });
+    }catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message
+        });
+
+    }
+};
+
+
+const getPlayerById=async(req,res)=>{
+    try{
+        const player=await Player.findById(req.params.id);
+        if(!player){
+            return res.status(404).json({
+                success:false,
+                message:"Player not found"
+            });
+        }
+        res.status(200).json({
+            success:true,
+            data:player
+        });
+    }catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message
+        });
+    }
+};
+
+const deletePlayer=async(req,res)=>{
+    try{
+        const player =await Player.findByIdAndDelete(req.params.id);
+        if(!player){
+            return res.status(404).json({
+                success:false,
+                message:"Player not found"
+            });
+        }
+        res.status(200).json({
+            success:true,
+            message:"Player deleted successfully"
+        });
+    }catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message
+        });
+    }
+};
+
+const updatePlayer=async(req,res)=>{
+    try{
+        const player=await Player.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new:true,
+                runValidators:true
+            }
+        );
+        if(!player){
+            return res.status(404).json({
+                success:false,
+                message:"Player not found"
+            });
+        }
+        res.status(200).json({
+            success:true,
+            data:player
+        });
+
+    }catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message
+        });
+    }
+};
+module.exports={ createPlayer,getAllPlayers,getPlayerById,deletePlayer,updatePlayer };
--- src/routes/playerRoutes.js
+++ src/routes/playerRoutes.js
@@ -1,6 +1,11 @@
 const express=require("express");
 const router=express.Router();
 
-const { createPlayer } = require("../controllers/playerController");
+const { createPlayer,getAllPlayers,getPlayerById,updatePlayer,deletePlayer } = require("../controllers/playerController");
 router.post("/",createPlayer);
+router.get("/",getAllPlayers);
+router.get("/:id",getPlayerById);
+router.put("/:id",updatePlayer);
+router.delete("/:id",deletePlayer);
+
 module.exports=router;
